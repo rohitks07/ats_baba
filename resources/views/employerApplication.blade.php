e6aa3da84ca7fc6c47907bc62ca5625a49fc7068
lokeashwarikumari <lokeshwari.itscient@gmail.com>
2020-02-07 20:09:58 +0530

    Update employerApplication.blade.php

--- resources/views/employerApplication.blade.php
+++ resources/views/employerApplication.blade.php
@@ -154,7 +154,7 @@
                                                 <td>{{$applied_date}}</td>
                                                 <td>
                                                     <a  onclick="interviewModalfunction({{$id}})" ><i class="fa fa-clock-o" aria-hidden="true" title="Schedule Interview"></i></a>
-                                                    <a href="{{url('employer/appli_del/'.$id)}}" class="hidden on-editing login-row" title="Delete"><i class="fa fa-trash-o"></i></a>
+                                                    <a href="{{url('employer/appli_del/'.$id)}}" class="hidden on-editing login-row" title="Delete" onclick="return confirm('Are you sure you want to delete this item?');"><i class="fa fa-trash-o"></i></a>
                                                     <a href="{{url('employer/appli_forward/'.$id)}}"><i class="fa fa-arrow-right" title="Candidate Forward "></i></a>
                                                     <a href="{{url('employer/application_view/'.$id)}}"> <i class="fa fa-eye" aria-hidden="true" title="View  Application"></i></a>
 
